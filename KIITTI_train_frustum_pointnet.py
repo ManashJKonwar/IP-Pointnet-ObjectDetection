cc5a45471e56d97fe77d058ec44926267068a468
ManashJKonwar <rickykonwar@gmail.com>
2022-02-14 21:10:28 +0530

    Update: Renaming_files
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ KIITTI_train_frustum_pointnet.py
@@ -0,0 +1,61 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2022, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
+
+import os
+from utility.utility_datatransformation import download_dataset
+from preprocessing.KITTI.generate_data import generate_data
+
+if __name__ == '__main__':
+    TRAIN_FRUSTUM_NET_KITTI = True
+    TRAIN_FRUSTUM_NET_LYFT = False
+
+    if TRAIN_FRUSTUM_NET_KITTI:
+        '''
+        Downloading KITTI Dataset
+        Consists of following datasets:
+            1. left color images
+            2. velodyne point clouds
+            3. camera calibration matrices
+            4. training labels
+        '''
+        kitti_dataset_directory =  download_dataset(
+                                        dataset_url_dict={
+                                            'left color images':'https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_image_2.zip',
+                                            'velodyne point clouds':'https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_velodyne.zip',
+                                            'camera calibration matrices':'https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_calib.zip',
+                                            'training labels':'https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_label_2.zip'
+                                        },
+                                        dataset_directory=os.getcwd(),
+                                        extracted_folder_name=r'KITTI'
+                                    )
+
+        '''
+        Data Preparation for KITTI Dataset
+        During this process, Extraction of frustum point clouds along with ground truth labels 
+        from the original KITTI data is performed which is based on both ground truth 2D bounding boxes 
+        and boxes from a 2D object detector. We will do the extraction for the train dataset under 
+        (datasets/KITTI/image_sets/train.txt) and validation sets under (datasets/KITTI/image_sets/val.txt)
+        using ground truth 2D boxes, and also extract data from validation set with predicted 2D boxes 
+        under (datasets/KITTI/rgb_detections/rgb_detection_val.txt).
+        This is initially done for only 3 object types namely
+        1. Car
+        2. Pedestrain and 
+        3. Cyclist
+        However, the behaviour can be extended for remaining objects as well
+        '''
+        generate_data(
+            dataset_directory=r'datasets\KITTI',
+            generate_train_data=True,
+            generate_val_data=True,
+            generate_rgb_val_data=True,
+            object_list=['Car', 'Pedestrian', 'Cyclist']
+        )
+
+        '''
+        '''
---
+++ KITTI_visualize_objects.py
@@ -0,0 +1,125 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2022, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
+
+import cv2
+import numpy as np
+import utility.KITTI.kitti_util as utils
+from utility.KITTI.kitti_object import kitti_object, show_image_with_boxes, show_lidar_with_boxes, show_lidar_on_image, get_lidar_in_image_fov
+
+def in_hull(p, hull):
+    from scipy.spatial import Delaunay
+    if not isinstance(hull,Delaunay):
+        hull = Delaunay(hull)
+    return hull.find_simplex(p)>=0
+
+def extract_pc_in_box3d(pc, box3d):
+    ''' pc: (N,3), box3d: (8,3) '''
+    box3d_roi_inds = in_hull(pc[:,0:3], box3d)
+    return pc[box3d_roi_inds,:], box3d_roi_inds
+
+def visualize_objects(dataset_idx=0):
+    """
+    This method helps us to visualize 2d bounding box and 3d bounding boxes around
+    rgb image data and also helps us to visualize point clouds on image as well as 
+    in a 3d space and at the end also visualize objects point clouds by encompassing
+    a frustum around the object
+    Parameters: 
+        dataset_idx (int): training sample to consider for visualization
+    Returns: 
+        None
+    """
+    import mayavi.mlab as mlab
+    from utility.KITTI.viz_util import draw_lidar, draw_lidar_simple, draw_gt_boxes3d
+    dataset = kitti_object(r'datasets\KITTI')
+    data_idx = dataset_idx
+
+    # Load data from dataset
+    objects = dataset.get_label_objects(data_idx)
+    objects[0].print_object()
+    img = dataset.get_image(data_idx)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+    img_height, img_width, img_channel = img.shape
+    print(('Image shape: ', img.shape))
+    pc_velo = dataset.get_lidar(data_idx)[:,0:3]
+    calib = dataset.get_calibration(data_idx)
+
+    ## Draw lidar in rect camera coord
+    #print(' -------- LiDAR points in rect camera coordination --------')
+    #pc_rect = calib.project_velo_to_rect(pc_velo)
+    #fig = draw_lidar_simple(pc_rect)
+    #raw_input()
+
+    # Draw 2d and 3d boxes on image
+    print(' -------- 2D/3D bounding boxes in images --------')
+    show_image_with_boxes(img, objects, calib)
+    # raw_input()
+
+    # Show all LiDAR points. Draw 3d box in LiDAR point cloud
+    print(' -------- LiDAR points and 3D boxes in velodyne coordinate --------')
+    #show_lidar_with_boxes(pc_velo, objects, calib)
+    #raw_input()
+    show_lidar_with_boxes(pc_velo, objects, calib, True, img_width, img_height)
+    # raw_input()
+
+    # Visualize LiDAR points on images
+    print(' -------- LiDAR points projected to image plane --------')
+    show_lidar_on_image(pc_velo, img, calib, img_width, img_height) 
+    # raw_input()
+    
+    # Show LiDAR points that are in the 3d box
+    print(' -------- LiDAR points in a 3D bounding box --------')
+    box3d_pts_2d, box3d_pts_3d = utils.compute_box_3d(objects[0], calib.P) 
+    box3d_pts_3d_velo = calib.project_rect_to_velo(box3d_pts_3d)
+    box3droi_pc_velo, _ = extract_pc_in_box3d(pc_velo, box3d_pts_3d_velo)
+    print(('Number of points in 3d box: ', box3droi_pc_velo.shape[0]))
+
+    fig = mlab.figure(figure=None, bgcolor=(0,0,0),
+        fgcolor=None, engine=None, size=(1000, 500))
+    draw_lidar(box3droi_pc_velo, fig=fig)
+    draw_gt_boxes3d([box3d_pts_3d_velo], fig=fig)
+    mlab.show()
+    # raw_input()
+    
+    # UVDepth Image and its backprojection to point clouds
+    print(' -------- LiDAR points in a frustum from a 2D box --------')
+    imgfov_pc_velo, pts_2d, fov_inds = get_lidar_in_image_fov(pc_velo,
+        calib, 0, 0, img_width, img_height, True)
+    imgfov_pts_2d = pts_2d[fov_inds,:]
+    imgfov_pc_rect = calib.project_velo_to_rect(imgfov_pc_velo)
+
+    cameraUVDepth = np.zeros_like(imgfov_pc_rect)
+    cameraUVDepth[:,0:2] = imgfov_pts_2d
+    cameraUVDepth[:,2] = imgfov_pc_rect[:,2]
+
+    # Show that the points are exactly the same
+    backprojected_pc_velo = calib.project_image_to_velo(cameraUVDepth)
+    print(imgfov_pc_velo[0:20])
+    print(backprojected_pc_velo[0:20])
+
+    fig = mlab.figure(figure=None, bgcolor=(0,0,0),
+        fgcolor=None, engine=None, size=(1000, 500))
+    draw_lidar(backprojected_pc_velo, fig=fig)
+    # raw_input()
+
+    # Only display those points that fall into 2d box
+    print(' -------- LiDAR points in a frustum from a 2D box --------')
+    xmin,ymin,xmax,ymax = \
+        objects[0].xmin, objects[0].ymin, objects[0].xmax, objects[0].ymax
+    boxfov_pc_velo = \
+        get_lidar_in_image_fov(pc_velo, calib, xmin, ymin, xmax, ymax)
+    print(('2d box FOV point num: ', boxfov_pc_velo.shape[0]))
+
+    fig = mlab.figure(figure=None, bgcolor=(0,0,0),
+        fgcolor=None, engine=None, size=(1000, 500))
+    draw_lidar(boxfov_pc_velo, fig=fig)
+    mlab.show()
+    # raw_input()
+
+if __name__ == '__main__':
+    visualize_objects(dataset_idx=0)
